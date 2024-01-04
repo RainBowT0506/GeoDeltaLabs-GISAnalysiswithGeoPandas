3b36d5fc6071e968764b0a8ec611fa948b25b69e
RainBowT <rainbowt0506@gmail.com>
2024-01-04 15:52:48 +0800

    feat: Import and plot AOI shapefile using GeoPandas

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import matplotlib.pyplot as plt
 # Importing and plotting the cities shapefile 
 cities = gpd.read_file('./Shapefiles/belgian_cities.shp')
 cities.plot()
-plt.title('Default Cities Shapefile Plot')
+plt.title('Belgian Cities Plot - Default')
 plt.show()
 
-cities.plot(cmap='jet', column='NAME_4', figsize=(15, 15))
-plt.title('Cities Shapefile Plot with Custom Styling')
+cities.plot(cmap='jet')
+plt.title('Belgian Cities Plot - Colormap (jet)')
+plt.show()
+
+
+cities.plot(cmap='jet', column='NAME_4', figsize=(10, 10))
+plt.title('Belgian Cities Plot - Column (NAME_4) and Colormap (jet)')
+plt.show()
+
+# Importing and plotting AOI shapefile
+AOI = gpd.read_file('./Shapefiles/area_of_interest_.shp')
+AOI.plot()
 plt.show()
