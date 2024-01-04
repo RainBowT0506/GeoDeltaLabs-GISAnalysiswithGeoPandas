02f73883278f6ba5fd55d94c80f32e93c6ae5048
RainBowT <rainbowt0506@gmail.com>
2024-01-04 16:08:10 +0800

    feat: Perform spatial intersection of Belgian cities with AOI using GeoPandas, calculate area, and save results to 'intersected_cities.shp'

--- main.py
+++ main.py
@@ -27,3 +27,15 @@ cities.plot(ax=ax, cmap = 'rainbow', column = 'NAME_4')
 AOI.plot(ax=ax, facecolor = 'yellow')
 plt.title('Belgian Cities with AOI Plot')
 plt.show()
+
+# Intersecting
+cities_in_AOI = gpd.overlay(cities, AOI, how = 'intersection')
+cities_in_AOI.plot(figsize = (10,10),cmap='jet')
+# Assigning a new column - Area
+cities_in_AOI['Area(km2)'] = cities_in_AOI.area/1000000
+
+plt.title('Intersected Cities within Area of Interest (AOI)')
+plt.show()
+
+# Save the geodataframe to a .shp (Shapefile)
+cities_in_AOI.to_file('./Shapefiles/intersected_cities.shp')
